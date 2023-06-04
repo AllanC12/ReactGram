fa8c5c2bdc0e191229041b898891610bf446a51b
AllanC12 <allandevfront@gmail.com>
2023-06-03 23:19:04 -0300

    iniciando front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom"
+
+//components
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+//pages
+import Home from './pages/home/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+       <Navbar/>
+        <Routes>
+           <Route path="/" element={<Home/>}/>
+           <Route path="/login" element={<Login/>}/>
+           <Route path="/register" element={<Register/>}/>
+        </Routes>
+       <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
