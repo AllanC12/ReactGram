4e7cd51852db30ab29bd73c19d31af3e0b650ccf
AllanC12 <allandevfront@gmail.com>
2023-06-26 07:17:55 -0300

    service de atualização de fotos adicionado

--- src/services/photoService.js
+++ src/services/photoService.js
@@ -40,10 +40,22 @@ const deletePhoto = async (id, token) => {
   }
 };
 
+const updatePhoto = async (data, id) => {
+  const config = requestConfig("PUT", data, token);
+  try {
+    const res = await fetch(api + "/photos/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
-  deletePhoto
+  deletePhoto,
 };
 
 export default photoService;
